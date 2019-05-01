c97871f02145a8f65a6c4aabfcbb43fe4fcfb661
pverspeelt <phiver4@gmail.com>
2019-05-01 14:46:23 +0200

    Add TagValue object
    Add TagValue object in objects.R file. Needed for sending and processing placeOrder messages.

---
+++ R/objects.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+TagValue <- function(tag = NA_character_,
+                     value = NA_character_){
+  structure(
+    list(tag = tag,
+         value = value
+         )
+  )
+}
