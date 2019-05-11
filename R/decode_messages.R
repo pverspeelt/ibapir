044849a927cd6d7050cd511a7f816cce705eaccc
pverspeelt <phiver4@gmail.com>
2019-05-11 17:13:44 +0200

    Add reqNewsArticle function
    Add reqNewsArticle function to reqNewsProviders.R file.
    
    Add processNewsArticleMsg function to process the return message.
    
    Add function to namespace.
    
    Add first draft of reqNewsArticle documentation.

--- R/decode_messages.R
+++ R/decode_messages.R
@@ -14,7 +14,7 @@ ProcessMsg <- function(in_msg, ib_con) {
   msgId <- as.integer(in_msg[msg_counter()])
 
   # Switch between function calls. Order in function calls should always be
-  # in_msg, msg_counter, ib_con
+  # in_msg, msg_counter, ib_con if ib_con is needed.
   switch(names(which(.incoming_msg_id == msgId)),
          ERR_MSG = processErrMsg(in_msg, msg_counter, ib_con),
          MANAGED_ACCTS = processManagedAcctsMsg(in_msg, msg_counter),
@@ -24,6 +24,7 @@ ProcessMsg <- function(in_msg, ib_con) {
          CONTRACT_DATA_END = processContractDataEndMsg(in_msg, msg_counter, ib_con),
          SYMBOL_SAMPLES = processSymbolSamplesMsg(in_msg, msg_counter),
          NEWS_PROVIDERS = processNewsProvidersMsg(in_msg, msg_counter),
+         NEWS_ARTICLE = processNewsArticleMsg(in_msg, msg_counter),
          warning(glue("Unknown incoming message Id: {msgId}
                       with message: {paste0(in_msg, collapse = \" \")}."), call.=FALSE)
          )
--- R/reqNewsProviders.R
+++ R/reqNewsProviders.R
@@ -58,3 +58,71 @@ processNewsProvidersMsg <- function(in_msg, msg_counter) {
 }
 
 
+#' Request News Article Body
+#'
+#' Requests news article body given articleId
+#'
+#' @param reqId id to specify the request. Should be a scalar integer.
+#' @param providerCode Short code indicating news provider, e.g. FLY
+#' @param articleId id of the specific article
+#' @param ib_con A valid ib connection.
+#'
+#' @return a data.frame containing the article type and article text
+#' @export
+#'
+#' @examples
+#' Example to follow
+reqNewsArticle <- function(reqId,
+                           providerCode,
+                           articleId,
+                           ib_con){
+
+  if (ib_con$server_version < .server_version$MIN_SERVER_VER_REQ_NEWS_ARTICLE){
+    stop(glue("Current server version {ib_con$server_version} does not support news article request."),
+         call. = FALSE)
+  }
+
+  out_msg <- c(make_field(.outgoing_msg_id$REQ_NEWS_ARTICLE),
+               make_field(reqId),
+               make_field(providerCode),
+               make_field(articleId))
+
+  # send newsArticleOptions parameter
+  if (ib_con$server_version  >= .server_version$MIN_SERVER_VER_NEWS_QUERY_ORIGINS) {
+    newsArticleOptionsStr = ""
+  } # reserved for internal use. Should be defined as null.
+
+  out_msg = c(out_msg,
+              make_field(newsArticleOptionsStr))
+
+  send_message(out_msg, ib_con)
+
+  # receive messages
+  while (TRUE) {
+    if (socketSelect(list(ib_con$con), FALSE, 0.1)) {
+      in_msg <- parse_incoming_message(ib_con$con)
+      msgId <- in_msg[1]
+
+      if (msgId == .incoming_msg_id$NEWS_ARTICLE) {
+        article <- ProcessMsg(in_msg, ib_con)
+      } else
+        if (msgId == .incoming_msg_id$ERR_MSG) {
+          ProcessMsg(in_msg, ib_con)
+        }
+    } else
+      break
+  }
+  return(article)
+}
+
+#' @keywords internal
+processNewsArticleMsg <- function(in_msg, msg_counter){
+
+  reqId = decoder("integer", in_msg[msg_counter()])
+  articleType = decoder("integer", in_msg[msg_counter()])
+  articleText = in_msg[msg_counter()]
+
+  article <- data.frame(articleType, articleText)
+
+}
+
